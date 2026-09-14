0dad038428a437b5a4cc05c5d96a457301559cd2
Christian Cahig <christian.cahig@outlook.com>
2026-09-14 12:14:51 +0800

    `act-02` : Update template script

--- materials/act-02/act-02.py
+++ materials/act-02/act-02.py
@@ -9,4 +9,11 @@ https://github.com/christian-cahig/CVE154_AY-2026-2027-S1.
 """
 
 if __name__ == "__main__":
-    pass
+
+    print(f"4 panels @ {panel_len} ft; hinge @ H = {hinge_loc} ft")
+    print(f"{load_mag} kip @ x = {load_loc} ft")
+    print(f"{" "*3}Ay = {Ay(load_mag, load_loc, P=panel_len, H=hinge_loc):.5f} kip")
+    print(f"{" "*3}Ey = {Ey(load_mag, load_loc, P=panel_len, H=hinge_loc):.5f} kip")
+    print(f"{" "*3}Em = {Em(load_mag, load_loc, P=panel_len, H=hinge_loc):.5f} kip")
+    print(f"{" "*3}Fy = {Fy(load_mag, load_loc, P=panel_len, H=hinge_loc):.5f} kip")
+    print(f"{" "*3}mu_G = {mu_G(load_mag, load_loc, P=panel_len, H=hinge_loc):.5f} kip-ft")
