6da0b69b43cd627db86a194fd7332e2be34ea6eb
Christian Cahig <christian.cahig@outlook.com>
2026-09-24 09:48:28 +0800

    `act-03` : Add template script

---
+++ materials/act-03/act-03.py
@@ -0,0 +1,46 @@
+"""
+CVE154 (A.Y. 2026-2027, S1)
+Template script for Programming Activity No. 03
+
+Prepared by Christian Cahig
+
+This file belongs to the repository
+https://github.com/christian-cahig/CVE154_AY-2026-2027-S1.
+"""
+
+if __name__ == "__main__":
+    # Part 2-1
+    
+    print("Part 2-1. Bisection")
+    print(f"{' '*3}rel. rough.: {0.0123456789}")
+    print(f"{' '*3}Reynolds n.: {9876.543210}")
+    print(f"{' '*3}init. guess: {x0_bs}")
+    print(f"{' '*3}max. iters.: {K_bs}")
+    print(f"{' '*3}z({x_bs}) = {z_bs(x_bs)} in {x_bs_info.iterations} iters.")
+
+    # Part 2-1
+    
+    print("Part 2-2. Newton-Raphson")
+    print(f"{' '*3}rel. rough.: {0.0456789123}")
+    print(f"{' '*3}Reynolds n.: {6789.543210}")
+    print(f"{' '*3}init. guess: {x0_nr}")
+    print(f"{' '*3}max. iters.: {}")
+    print(f"{' '*3}z({x_nr}) = {} in {} iters.")
+
+    # Part 2-3
+        
+    print("Part 2-3. Secant")
+    print(f"{' '*3}rel. rough.: {0.0213789456}")
+    print(f"{' '*3}Reynolds n.: {4567.890123}")
+    print(f"{' '*3}init. guess: {x0_sc}")
+    print(f"{' '*3}max. iters.: {}")
+    print(f"{' '*3}z({x_sc}) = {} in {} iters.")
+
+    # Part 2-4
+        
+    print("Part 2-4. TOMS Algorithm 748")
+    print(f"{' '*3}rel. rough.: {0.0321789456}")
+    print(f"{' '*3}Reynolds n.: {12345.6789}")
+    print(f"{' '*3}init. guess: {}")
+    print(f"{' '*3}max. iters.: {}")
+    print(f"{' '*3}z({x_to}) = {} in {} iters.")
